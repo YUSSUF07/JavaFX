0bfa71c736db433fa9a462491c54f4c83ddc799d
Youssouf Faye <fayey@MATINF.uqtr.ca>
2023-04-09 12:56:01 -0400

    controller

--- Projet_UI/src/main/java/com/fx/projet_ui/AcceuilController.java
+++ Projet_UI/src/main/java/com/fx/projet_ui/AcceuilController.java
@@ -7,9 +7,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class AcceuilController {
-    Stage stage;
-    Scene scene;
     @FXML
     private Button btn_client;
     @FXML
@@ -18,9 +18,29 @@ public class AcceuilController {
 
     public void onConnexionButtonClick()
     {
-
+        Stage employe = new Stage();
+        try{
+            Parent fxml = FXMLLoader.load(getClass().getResource("Connexion.fxml"));
+            Scene scene1 = new Scene(fxml);
+            employe.setScene(scene1);
+            employe.setTitle("Employe");
+            employe.show();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
     public void onClientButtonClick(){
-
+        Stage client = new Stage();
+        try{
+            Parent fxml = FXMLLoader.load(getClass().getResource("Client.fxml"));
+            Scene scene1 = new Scene(fxml);
+            client.setScene(scene1);
+            client.setTitle("Employe");
+            client.show();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 }
